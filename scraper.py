de2702596fa6116f78cee7173b1435220fb4c463
zhcaury2 <zhcaury2@gmail.com>
2026-02-03 21:16:59 -0800

    Added like 4 lines to extract function

--- scraper.py
+++ scraper.py
@@ -1,10 +1,12 @@
 import re
 from urllib.parse import urlparse
 
+# From Beautiful Soup library, must install with pip
+from bs4 import BeautifulSoup
+
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
-
 def extract_next_links(url, resp):
     # Implementation required.
     # url: the URL that was used to get the page
@@ -15,6 +17,16 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+
+    if resp.status != 200:
+        return list()
+    
+    urls = []
+    content = BeautifulSoup(resp.raw_response.content, "html.parser")
+
+
+
+    
     return list()
 
 def is_valid(url):
